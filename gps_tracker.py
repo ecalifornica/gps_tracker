c6093c68e7841d83e5f2f842f131a8155065ecd0
Robert Queenin <queenin@gmail.com>
2016-02-01 00:21:23 -0800

    gps to flat file

--- gps_tracker.py
+++ gps_tracker.py
@@ -1,41 +1,24 @@
-import pickle
 import serial
 import time
 
-import Adafruit_BBIO.UART as UART
 import pynmea2
-#UART.setup("UART4")
-GPS = serial.Serial('/dev/ttyO4', 9600)
 
 
 def read_gps():
-    while GPS.inWaiting()==0:
-        pass
-    barf=GPS.readline()
-    print barf
-    if barf[:6] == '$GPGGA':
-        with open('/var/tmp/gps_track.txt', 'a') as fh:
-            msg = pynmea2.parse(barf)
-            print(msg.latitude, msg.longitude)
-            # print(help(msg))
-            #fh.write(pickle.dumps(msg))
-            pickle.dump(msg, fh)
-
-def read_gps_2():
-    GPS.reset_input_buffer()
+    gps_conn = serial.Serial('/dev/ttyO4', 9600)
     while True:
-        data = GPS.readline()
-        if data.startswith("$GPGGA"):
+        data = gps_conn.readline()
+        if data.startswith('$GPGGA'):
             msg = pynmea2.parse(data)
-            #print(repr(msg))
-            #print(dir(msg))
-            #print(msg.data, msg.fields)
-            #GPS.reset_input_buffer()
-            #GPS.flushOutput()
+            # GPS.reset_input_buffer()
             return msg.timestamp, msg.latitude, msg.longitude
+    gps_conn.close()
 
-while True:
-    result = read_gps_2()
-    print(result[0].isoformat(), result[1], result[2])
-    time.sleep(5)
 
+if __name__ == '__main__':
+    while True:
+        ts, lat, lon = read_gps()
+        with open('/var/tmp/gps_track.txt', 'a+') as file_handle:
+            print(ts.isoformat(), lat, lon)
+            file_handle.write('{}, {}, {}\n'.format(ts.isoformat(), lat, lon))
+        time.sleep(9)
