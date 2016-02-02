2b8833ea023eaccf8eceb215ef12501b1211237d
ecalifornica <queenin+git@gmail.com>
2016-02-01 16:09:55 -0800

    testing gps tracker api

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def hi():
+    if request.method == 'POST':
+        # print request.data
+        print request.json
+        return 'accepted'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
