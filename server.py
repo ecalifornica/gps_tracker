d5aa9920bfcd93aa1d52ebacb21fad92abf4d39b
Robert Queenin <queenin@gmail.com>
2016-02-02 01:01:25 -0800

    cleaning up json, should do this at the nodes

--- server.py
+++ server.py
@@ -1,15 +1,36 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import pymongo
 
 app = Flask(__name__)
+mc = pymongo.MongoClient()
+db = mc.gps_test
+collection = db.test_col
 
 @app.route('/gps', methods=['GET', 'POST'])
-def hi():
+def coordinate_upload():
+    if request.method == 'GET':
+        return 'hello world'
     if request.method == 'POST':
         # print request.data
-        print request.json
-
+        data = request.json
+        gps_points = []
+        for i in data:
+            lon = float(data[i][1].rstrip())
+            lat = float(data[i][0])
+            doc = {'timestamp': i, 'coord': [lon, lat]}
+            print(doc)
+            gps_points.append(doc)
+        mc_result = collection.insert_many(gps_points)
+        #print(dir(mc_result))
+        print(mc_result.inserted_ids)
+        
         return 'accepted'
+
+
+@app.route('/track')
+def track():
+    return render_template('track.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
